f6ac8d77493598e417143a4f2b72d71f1a70fa7f
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-01-14 19:16:30 +0400

    Add logout

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,64 @@
-import React from "react";
-import { FaGoogle } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import { FaGoogle, FaSignOutAlt } from "react-icons/fa";
 import { auth, provider } from "../app/fbconfig";
 import { signInWithPopup } from "firebase/auth";
+
 export default function Navbar() {
-  const handleClick = async () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setLoggedIn(true);
+        setUser(user);
+      } else {
+        setLoggedIn(false);
+        setUser(null);
+      }
+    });
+  }, []);
+
+  const handleLogin = async () => {
     const result = await signInWithPopup(auth, provider);
+    setUser(result.user);
+    setLoggedIn(true);
   };
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    setUser(null);
+    setLoggedIn(false);
+  };
+
   return (
     <nav className="bg-blue-500">
-      <div className="flex items-center justify-start p-4">
-        <button
-          className="flex bg-white text-blue-500 font-medium p-2 rounded-lg 
-        justify-items-center hover:bg-blue-800 hover:text-white transition-left 
-        duration-500 ease-in-out"
-          onClick={handleClick}
-        >
-          <p className="mr-1">Login with</p> <FaGoogle className="my-auto" />
-        </button>
+      <div className="flex p-4 justify-center">
+        {loggedIn ? (
+          <div className="flex">
+            <div className="flex items-center mr-3 text-white">
+              {user ? "Welcome, " + user.displayName : ""}
+            </div>
+            <button
+              className="flex bg-white text-blue-500 font-medium p-2 rounded-lg 
+            justify-items-center hover:bg-blue-800 hover:text-white transition-left 
+            duration-500 ease-in-out"
+              onClick={handleLogout}
+            >
+              <p className="mr-1">Logout</p>{" "}
+              <FaSignOutAlt className="my-auto" />
+            </button>
+          </div>
+        ) : (
+          <button
+            className="flex bg-white text-blue-500 font-medium p-2 rounded-lg 
+            justify-items-center hover:bg-blue-800 hover:text-white transition-left 
+            duration-500 ease-in-out"
+            onClick={handleLogin}
+          >
+            <p className="mr-1">Login with</p> <FaGoogle className="my-auto" />
+          </button>
+        )}
       </div>
     </nav>
   );
